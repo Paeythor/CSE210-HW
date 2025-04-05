6860f7c0219798d0ea459ef65f4100070a8bf77c
Paeythor <theschumann@byui.edu>
2025-04-05 17:33:53 -0600

    another typo

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -18,7 +18,7 @@ class Program
                 Console.WriteLine("Now leaving the program...");
                 break;
             }
-            Console.WriteLine("What portion of the text percentage-wise to start 1-100 would you like to start with?");
+            Console.WriteLine("What portion of the text percentage-wise (1-100) would you like to start with?");
             int startDifficulty = int.Parse(Console.ReadLine());
             Console.WriteLine("Thank you so much for your entry. Seeking your scripture diligently.");
 
