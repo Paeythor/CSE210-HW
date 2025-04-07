18a16058d24b9b0a04f9c8f83846d70dd0841441
Paeythor <theschumann@byui.edu>
2025-04-07 07:53:13 -0600

    update and correction

--- week05/Homework/Assignment.cs
+++ week05/Homework/Assignment.cs
@@ -7,22 +7,14 @@ public class Assignment
     {
         _studentName = studentName;
         _topic = topic;
+    // Get a summary of the assignment
+    }public string GetSummary() 
+    {
+        return $"{_studentName} - {_topic}";
     }
-
-    // We will provide Getters for our private member variables so they can be accessed
-    // later both outside the class as well is in derived classes.
-    public string GetStudentName()
+    // Protected getter for _studentName, to be used in derived classes
+    protected string GetStudentName()
     {
         return _studentName;
-    }
-
-    public string GetTopic()
-    {
-        return _topic;
-    }
-
-    public string GetSummary()
-    {
-        return _studentName + " - " + _topic;
     }
 }
